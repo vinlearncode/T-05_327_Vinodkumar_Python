2a5872a2f9be186def62673dc2b6b48e58a7566b
vinod <104897706+Nanivino@users.noreply.github.com>
2022-09-01 15:47:12 +0530

    added variables_2

--- 02_Variables/Variables.py
+++ 02_Variables/Variables.py
@@ -1,42 +1,42 @@
 # Variables in Python
 
-first_name = 'vinod'
-last_name = 'kumar'
-country = 'canada'
-city = 'Helsinki'
-age = 25
-is_married = False
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'vinod',
-    'lastname':'kumar',
-    'country':'canada',
-    'city':'Helsinki'
-    }
-
-# Printing the values stored in the variables
-
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
-
-# Declaring multiple variables in one line
-
-first_name, last_name, country, age, is_married = 'vinod', 'kumar', 'helsinki', 25, False
-
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+# first_name = 'vinod'
+# last_name = 'kumar'
+# country = 'canada'
+# city = 'Helsinki'
+# age = 25
+# is_married = False
+# skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+# person_info = {
+#     'firstname':'vinod',
+#     'lastname':'kumar',
+#     'country':'canada',
+#     'city':'Helsinki'
+#     }
+#
+# # Printing the values stored in the variables
+#
+# print('First name:', first_name)
+# print('First name length:', len(first_name))
+# print('Last name: ', last_name)
+# print('Last name length: ', len(last_name))
+# print('Country: ', country)
+# print('City: ', city)
+# print('Age: ', age)
+# print('Married: ', is_married)
+# print('Skills: ', skills)
+# print('Person information: ', person_info)
+#
+# # Declaring multiple variables in one line
+#
+# first_name, last_name, country, age, is_married = 'vinod', 'kumar', 'helsinki', 25, False
+#
+# print(first_name, last_name, country, age, is_married)
+# print('First name:', first_name)
+# print('Last name: ', last_name)
+# print('Country: ', country)
+# print('Age: ', age)
+# print('Married: ', is_married)
 
 #  x = 10.  CRUD operations
 #     x=10----> Create
@@ -47,4 +47,15 @@ print('Married: ', is_married)
 # Tokens in Python. Explain all types
 #     1.	A token is the smallest individual unit in python program. All statements and instructions in a program are built with tokens.
 #     2. The token types are literals,Operators,keywords,Identifiers and constant.
+# static way:-
+# class Example:
+#         staticVariable = 5 # Access through class
+#
+# print (Example.staticVariable)
+
+# Dynamic Way: -
+# var = 'b'
+# value = 6
+# dict2 = {var: value}
+# print(dict2['b'])
 
