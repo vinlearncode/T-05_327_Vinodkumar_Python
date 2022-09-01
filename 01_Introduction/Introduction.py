6ee122f7732aa58fe5bd700706fb2ed526856f31
vinod <104897706+Nanivino@users.noreply.github.com>
2022-09-01 14:19:17 +0530

    added introduction

---
+++ 01_Introduction/Introduction.py
@@ -0,0 +1,90 @@
+# I. Introduction:
+# =================
+# 1. Tell me about Python?
+#     *python is introduced by guido van russom in 1991.
+#     *python is both functional programming like C,pascal and object oriented programming language like c++,java
+#     *python is procedural and object oriented programming language
+#     *python is highlevel language and dynamicaly typed language
+#     *python has rich libraries like numpy,pandas,matplotlib,seaborn,skcitlearn
+#
+# 2. Why Python is so popular now a days?
+#     *python used to be great effect in web development,data analysis,data science,machinelearning and artificial intelligence.
+#     *python is widely used in IOT Technology
+#     *python is easy to learn
+#     *Big data machinelearning and cloudcomputing
+#     *flexibility of python language
+#         a)The person who is an expert in python language is not just limited to build similar kinds of things but can also go
+#         on to try to make something different than before.
+#
+# 3. Features of Python
+#     *simple when we read a python program we feel like reading english.
+#     *Easy to learn, python uses very few keywords.
+#     *python resembles c language so migrating from c to python is easy for programmers.
+#     *open source,python can be freely downloaded and There is no pay for python software.
+#     *High level programming language,Human readable language.
+#     *Dynamicaly typed,In python,we need not to declare anything.
+#     *C and java are statiscally typed but python is dynamically typed language.
+#     *platform independent,when a python program is compiled using a compiler,it generates byte code.Anybody can run these byte
+#      code instructions on any computer using PVM,so python is independent python runs on almost all operating systems like Unix,
+#      linux,windows,mac etc..
+#     *portable,when a program yields same output on any computer in the world it is called portable.
+#
+# 4. Advantages and Disadvantages of Python
+#     Advantages:-
+#     *It is easy to learn and use and it has an extensive library
+#     *It increased productivity:
+#         To understand the syntax and behavior of the programming language, the users do not have to spend hours, so more work
+#         is done.
+#     *Flexibility and extensive library:
+#         This language is very flexible, and hence it allows the user to try new things. The users can develop
+#         new sorts of the application using Python programming language.
+#     *supportive community:
+#         There are enough guides, tutorials, and documentation available on the Python programming language, which helps the
+#         developers to understand the language faster and better. Because of its supportive community, Python has rapid growth
+#         compared to other languages.
+#
+# 5. Interpreted vs Compiled time programming languages. Explain in detail
+#     Compiled time:-
+#         Compiled languages are converted directly into machine code that the processor can execute. As a result, they tend to be faster and more efficient to
+#         execute than interpreted languages.
+#     Interpreted time:-
+#         In Interpreted languages, the program cannot be compiled, it is interpreted.
+#         The interpreted programs run slower than the compiled program.
+#         An interpreted language is a language in which the implementations execute instructions directly without earlier compiling a program into machine language.
+#
+# 6. .py vs .pyc files
+#     .py is source code and .pyc is byte code
+#
+# 7. How compilation will happen internally. Explain in detail
+#     Python converts the source code into a series of byte codes. So within python, compilation stage happens, but directly into byte code and this byte code can't
+#     be identified by CPU. So there is a need for a mediator to do this task. Here an interpreter comes into existence called the python virtual machine
+#
+# 8. Why Python is Dynamically typed programming Language. Explain
+#     Python is a dynamically typed language. It doesn’t know about the type of the variable until the code is run. So declaration is of no use.
+#     It stores the value at some memory location and binds that variable to the memory container.
+#
+# 9. Python is Platform independent.Explain
+#     platform independent,when a python program is compiled using a compiler,it generates byte code.Anybody can run these byte code instuctions
+#     on any computer using PVM,so python is independent python runs on almost all operating systems like Unix,linux,windows,mac etc..
+#
+# 10.Different ways to write python program.
+#    Interactive, IDLE, CommandPrompt, IDE
+#    Advantages, Disadvantages
+#
+#    Interactive:-
+#    Interactive mode is a command line shell which gives immediate feedback for each statement, while running previously fed statements in active memory.
+#    As new lines are fed into the interpreter, the fed program is evaluated both in part and in whole.
+#    IDLE:-
+#    IDLE can be used to execute a single statement just like Python Shell and also to create, modify, and execute Python scripts. IDLE provides
+#    a fully-featured text editor to create Python script that includes features like syntax highlighting, autocompletion, and smart indent.
+#    CommandPrompt:-
+#     In the interactive mode, we saw that as we write the command so does it asks for the input in the very next line. But in script mode we first code the
+#     entire program save and then run it in command prompt. The python interpreter executes the code line by line and gives us the result accordingly.
+#     IDE:-
+#     An IDE or an Integrated Development Environment is an application that is used to create software.
+#     The benefits of using an IDE are,
+#     Code editor: Designed for writing and editing source code, these editors are distinguished from text editors because work to either simplify or enhance the process of writing and editing of code for developers
+#     Compiler: Compilers transform source code that is written in a human readable/writable language in a form that computers can execute.
+#     Debugger: Debuggers are used during testing and can help developers debug their application programs.
+#     Build automation tools: These can help automate developer tasks that are more common to save time.
+#
