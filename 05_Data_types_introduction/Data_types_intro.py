903c2b47e6b41e38eb80981703bd9300b15c02ae
vinod <104897706+Nanivino@users.noreply.github.com>
2022-09-01 17:32:27 +0530

    Data_types_intro_added

---
+++ 05_Data_types_introduction/Data_types_intro.py
@@ -0,0 +1,73 @@
+# Data types:-
+#
+#  In programming, data type is an important concept.
+#
+# Variables can store data of different types, and different types can do different things.
+#
+# Python has the following data types built-in by default, in these categories:
+#
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# str
+x = "Hello World"
+print(x)
+print()
+# int
+x = 20
+print(x)
+print()
+# float
+x = 20.5
+print(x)
+print()
+# complex
+x = 1j
+print(x)
+print()
+# list
+x = ["apple", "banana", "cherry"]
+print(x)
+print()
+# tuple
+x = ("apple", "banana", "cherry")
+# range
+x = range(6)
+print(x)
+print()
+# dict
+x = {"name" : "John", "age" : 36}
+print(x)
+print()
+# set
+x = {"apple", "banana", "cherry"}
+print(x)
+print()
+# frozenset
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print()
+# bool
+x = True
+print(x)
+print()
+# bytes
+x = b"Hello"
+print(x)
+print()
+# bytearray
+x = bytearray(5)
+print(x)
+print()
+# memoryview
+x = memoryview(bytes(5))
+print(x)
+print()
+# NoneType
+x = None
